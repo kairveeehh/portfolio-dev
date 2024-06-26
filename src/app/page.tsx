011682100a38eb3ee30fe36c4a68b3ff20bd15e5
kairveeehh <jampu741@gmail.com>
2024-06-26 09:46:34 +0530

    added theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
+import { ModeToggle } from "@/components/ui/darkmode";
 
 export default function Home() {
   return (
-    <main >
-
-    <h1>portfolio begins</h1>
-      
-    
+    <main className="relative min-h-screen p-4">
+      <div className="absolute top-4 right-4">
+        <ModeToggle />
+      </div>
+      <h1>portfolio begins</h1>
     </main>
   );
 }
